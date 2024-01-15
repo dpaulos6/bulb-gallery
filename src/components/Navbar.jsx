eb842d587e1c0155405894658fdb99206f5ad6e9
Diogo Paulos <51181228+dpaulos6@users.noreply.github.com>
2024-01-15 11:18:35 +0000

    Update Navbar.jsx
    Update on User Icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -78,10 +78,9 @@ export const Navbar = () => {
                     target="_self"
                     className="text-white custom-border-gray hover-primary rounded-xl bg-customDarkBg2 hover:bg-customDarkBg3 p-3 text-md flex focus:outline-none focus-within:outline-none"
                   >
-                    <FaUser
-                      className='h-auto w-5 group-hover:text-gray-500'
-                      aria-hidden="true"
-                    />
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+                    </svg>
                   </a>
                   <button 
                     className="text-white custom-border-gray hover-primary rounded-xl bg-customDarkBg2 hover:bg-customDarkBg3  border-gray-700 px-6 pt-2 pb-2 text-md flex"
